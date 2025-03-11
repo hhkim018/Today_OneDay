a501b8d1fa57a662f7f6cf8f08c8304c5236d789
김현호 <hhkim628@naver.com>
2025-03-11 23:42:19 +0900

    sw 설정 및 구글 바로가기

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "my-app-cache-v1";
+const urlsToCache = ["/","/main_img.png"];
+
+// 서비스 워커 설치 (캐싱)
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 요청 가로채기 (캐시에서 제공)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 새로운 서비스 워커가 활성화되면 오래된 캐시 삭제
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
--- src/content/Image.jsx
+++ src/content/Image.jsx
@@ -1,8 +1,7 @@
-import mainImg from "../assets/main_img.png";
 const Image = () =>{
     return(
         <>
-        <img style={{width: "100%",height: "100%"}} src={mainImg}></img>
+        <img style={{width: "100%",height: "100%"}} src="/main_img.png"></img>
         </>
     )
 
