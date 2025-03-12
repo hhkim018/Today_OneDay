79f6930724c8eb47590158ee39b6090f08c37969
김현호 <hhkim@mqni.com>
2025-03-12 13:10:48 +0900

    localstorage 데이터 저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,43 @@
 import ExcelParser from "./utils/ExcelParser";
-import Image from "./content/Image"
-import Saying from "./content/Saying"
-import RequestSaying from "./content/RequestSaying"
-import {registerServiceWorker} from "./utils/ServiceWorker"
+import Image from "./content/Image";
+import Saying from "./content/Saying";
+import RequestSaying from "./content/RequestSaying";
+import { registerServiceWorker } from "./utils/ServiceWorker";
 import Alarm from "./utils/Alarm";
-import './App.css'
+import "./App.css";
 import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
-  const [saying,setSaying] = useState({word:"",people:""});
-  
-  useEffect(()=>{
-    ExcelParser().then(val=>{
-      setSaying(val);
-      Alarm(val);
-    })
-    registerServiceWorker();
+  const [saying, setSaying] = useState({ word: "", people: "" });
 
-  },[]);
+  useEffect(() => {
+    const now = new Date();
+    const isAfter9AM = now.getHours() >= 9;
+    console.log(isAfter9AM);
+
+    const toDayaying = localStorage.getItem("saying");
+    //TODO 매일 오전 9시에 초기화
+    if (toDayaying === null) {
+      ExcelParser().then((val) => {
+        localStorage.setItem("saying", JSON.stringify(val));
+        setSaying(val);
+        Alarm(val);
+      });
+    } else {
+      setSaying(JSON.parse(toDayaying));
+    }
+
+    registerServiceWorker();
+  }, []);
 
   return (
     <>
-    <Image/>
-    <Saying saying={saying}/>
-    {/* <RequestSaying/> */}
+      <Image />
+      <Saying saying={saying} />
+      {/* <RequestSaying/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/utils/ExcelParser.js
+++ src/utils/ExcelParser.js
@@ -1,23 +1,21 @@
-// 추후 양이 많아졌을 때 fast-csv 고려하기 
-import * as XLSX from "xlsx"
+// 추후 양이 많아졌을 때 fast-csv 고려하기
+import * as XLSX from "xlsx";
 
-const ExcelParser = async () =>{
+const ExcelParser = async () => {
+  const binaryData = await (await fetch("/goodword.xls")).arrayBuffer();
+  const workbook = XLSX.read(binaryData, { type: "array" }); // 엑셀 파일 읽기
+  const sheetName = workbook.SheetNames[0]; // 첫 번째 시트 선택
+  const sheet = workbook.Sheets[sheetName];
+  const saying = XLSX.utils.sheet_to_json(sheet); // JSON 변환
 
-     const binaryData = await (await fetch("/goodword.xls")).arrayBuffer()
-     const workbook = XLSX.read(binaryData, { type: "array" }); // 엑셀 파일 읽기
-     const sheetName = workbook.SheetNames[0]; // 첫 번째 시트 선택
-     const sheet = workbook.Sheets[sheetName];
-     const saying = XLSX.utils.sheet_to_json(sheet); // JSON 변환
-    
-     const sayingCnt = saying.length;
-     const sayingIdx = Math.ceil(Math.random() * sayingCnt);
+  const sayingCnt = saying.length;
+  const sayingIdx = Math.ceil(Math.random() * sayingCnt);
 
-     const row = Object.values(saying[sayingIdx])[0].split("-");
+  const row = Object.values(saying[sayingIdx])[0].split("-");
 
-     const word = row[0];
-     const people = row[1];      
-     return {word,people};      
-}
-
+  const word = row[0];
+  const people = row[1];
+  return { word, people };
+};
 
 export default ExcelParser;
