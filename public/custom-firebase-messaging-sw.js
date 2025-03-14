5a34542c42781961e9593022e5369298c4e734db
김현호 <hhkim628@naver.com>
2025-03-14 13:12:18 +0900

    FIreBase 설정 추가

---
+++ public/custom-firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+// self.addEventListener("message", (event) => {
+//   console.log(event);
+//   if (event.data && event.data.type === "INIT_FIREBASE") {
+//     // 런타임에 설정을 받아서 초기화
+//     const firebaseConfig = event.data.config;
+//     console.log(firebaseConfig);
+//     //   firebase.initializeApp(firebaseConfig);
+//   }
+// });
